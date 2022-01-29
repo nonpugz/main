6c535817810924fb99a3cb92e79b6234b3fd0fbb
NonPugz <>
2022-01-29 21:18:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Lets build a robot
+
+print("Hello welcome to my $Imple!!!!!")
+
+name = input("What is your name? ")
+
+print("\nHello " + name + ", thank you so much for coming in today\n")
+
+menu = "Chicken, Bread, Burger, HamBurger, ChessBurger\n"
+
+print(name + ", What would you like from our menu today? Here is what we are serving.\n" +  menu)
+
+order = input()
+
+print("\nSounds good " + name  + ", we have that " + order + " ready for you in a few mins ")
+
+
+
